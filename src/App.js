d7cd3c12a64a2fe5c495cca5380d00b97f76fac1
Krunal Patel <kppatel727@gmail.com>
2020-06-13 17:37:23 -0700

    display all route info in table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import DATA from './data.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.renderTableData = this.renderTableData.bind(this);
+  }
+
+  renderTableData() {
+    return DATA.routes.map((flight, idx) => {
+      const { airline, src, dest } = flight
+      return (
+        <tr key={idx}>
+          <td>{airline}</td>
+          <td>{src}</td>
+          <td>{dest}</td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="app">
         <header className="header">
           <h1 className="title">Airline Routes</h1>
         </header>
-        <section>
-          <p>
-            Welcome to the app!
-          </p>
-        </section>
+        <table>
+          <thead>
+            <tr>
+              <th>Airline</th>
+              <th>Source</th>
+              <th>Destination</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderTableData()}
+          </tbody>
+        </table>
       </div>
     );
   }
